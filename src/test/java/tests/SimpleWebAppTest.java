0723bcef7238d2740a79f761d1916126668e1c8b
Arunvarma1862 <arunbabu120894@gmail.com>
2026-05-21 16:43:39 +0530

    second commit

--- src/test/java/tests/SimpleWebAppTest.java
+++ src/test/java/tests/SimpleWebAppTest.java
@@ -1,5 +1,31 @@
 package tests;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class SimpleWebAppTest {
 
+    @Test
+    public void testWebApp() {
+
+        // ChromeDriver path
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\chromedriver\\chromedriver.exe");
+
+        // HEADLESS MODE (IMPORTANT for Jenkins)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9090/simple-webapp");
+
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
+    }
 }
